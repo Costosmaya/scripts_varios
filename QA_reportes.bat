18422ba0eb366308a7924dc3e35d314fb4df5e06
Francisco Alonzo <costos@mayaprin.com>
2022-04-08 07:30:03 -0600

    [add]: vaciar directorio antes de nuevos archivos

--- QA_reportes.bat
+++ QA_reportes.bat
@@ -1,4 +1,5 @@
 @echo off
+forfiles /P "C:\Users\User\Documents\Analisis  Desarrollo Costos\Scripts\Python\imgs_qa" /M * /C "cmd /c if @isdir==FALSE del @file"
 echo activate conda main
 "py" "c:/Users/User/Documents/Analisis  Desarrollo Costos/Scripts/Python/ocr_QA_envios.py"
 pause
